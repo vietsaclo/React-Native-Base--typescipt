1189ea0391eb70c23a4521452f68ab9025fbbdac
vietsaclo <vietsaclo@gmail.com>
2023-04-10 10:37:56 +0700

    Update env & chat gpt

--- actions/ChatGPT.ts
+++ actions/ChatGPT.ts
@@ -2,6 +2,10 @@ import { Configuration, OpenAIApi } from "openai";
 import Env from "../common/Env";
 
 class ChatGPT {
+    private configuration: any = null;
+    private openai: any = null;
+    private messageHistories: any[] = [];
+
     constructor() {
         this.configuration = new Configuration({
             apiKey: Env.OPENAI_API_KEY,
@@ -12,18 +16,18 @@ class ChatGPT {
         this.messageHistories = [];
     }
 
-    reply(message, onFinish) {
+    reply(message: string, onFinish: Function) {
         this.messageHistories.push({ role: "user", content: message });
         this.openai.createCompletion({
             model: "gpt-3.5-turbo",
             messages: this.messageHistories,
         })
-        .then((completion) => {
+        .then((completion: any) => {
             const completion_text = completion.data.choices[0].message.content;
             this.messageHistories.push({ role: "assistant", content: completion_text });
             onFinish(completion_text);
         })
-        .catch((_error) => {
+        .catch((_error: any) => {
             const messageError = `[${new Date().toUTCString()}] Đã ghi nhận "${message}" | Nhưng đã gặp sự cố, không trả lời được, hãy thự lại sau!`;
             onFinish(messageError);
         });
