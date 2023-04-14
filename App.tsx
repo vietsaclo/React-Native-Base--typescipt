5943249a8170f50c9e13c90bad4b2e07f1dbd7ed
vietsaclo <vietsaclo@gmail.com>
2023-04-14 14:06:54 +0700

    Update app.js

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Store from './common/Store';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import 'react-native-url-polyfill/auto';
 import Tikop from './scenes/Tikop';
+import store from './common/@core/Store';
 
 function App(): JSX.Element {
   return (
-    <Provider store={Store}>
+    <Provider store={store}>
       <SafeAreaProvider style={{ flex: 1 }}>
         <Tikop />
       </SafeAreaProvider>
