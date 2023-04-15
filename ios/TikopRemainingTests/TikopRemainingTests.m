5cffb697383797d758561d52bd6e4e5db29a1af2
vietsaclo <vietsaclo@gmail.com>
2023-04-15 11:23:11 +0700

    Rename app

--- ios/TikopRemainingTests/TikopRemainingTests.m
+++ ios/TikopRemainingTests/TikopRemainingTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeBaseTypescriptTests : XCTestCase
+@interface TikopRemainingTests : XCTestCase
 
 @end
 
-@implementation ReactNativeBaseTypescriptTests
+@implementation TikopRemainingTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
